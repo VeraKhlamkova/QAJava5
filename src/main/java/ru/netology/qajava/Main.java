e8c079e4f318d0508c9cee8f0077247a37af7e50
Vera Khlamkova <sovasov2015@gmail.com>
2023-10-10 17:06:03 +0300

    Initial Commit

---
+++ src/main/java/ru/netology/qajava/Main.java
@@ -0,0 +1,12 @@
+import ru.netology.qajava.BonusService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        long expected = 150;
+        long actual = service.calculate(5000, true);
+        System.out.println(expected + " == ? == " + actual);
+    }
+}
---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import ru.netology.qajava.BonusService;
+
+public class BonusServiceTest {
+
+    @Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+        long actual = service.calculate(amount, registered);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+        long actual = service.calculate(amount, registered);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForUnRegisteredUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10;
+        long actual = service.calculate(amount, registered);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForUnRegisteredAndNearOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500;
+        long actual = service.calculate(amount, registered);
+        Assertions.assertEquals(expected, actual);
+    }
+}
